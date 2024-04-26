db3a543944e71300c4d4471ff0d4c384abeaae76
AlexanderPRM <nathan920@yandex.ru>
2024-04-26 21:02:43 +0300

    Add strcspn and fix strncat

--- src/s21_strncat.c
+++ src/s21_strncat.c
@@ -1,12 +1,13 @@
 #include "s21_string.h"
 
 char *s21_strncat(char *dest, const char *src, size_t n) {
-  size_t curr = 0;
-  for (; dest[curr]; curr++) {
+  char *ptr = dest;
+  while (*ptr) {
+    ptr++;
   }
+
   for (size_t cnt = 0; src[cnt] != '\0' && cnt < n; cnt++) {
-    dest[curr] = src[cnt];
-    curr++;
+    *ptr++ = src[cnt];
   }
   return dest;
 }
