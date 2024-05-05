19659cb6ffc30e9e23b22e8d6f8412e232def105
AlexanderPRM <nathan920@yandex.ru>
2024-05-05 17:22:33 +0300

    first version of sprintf

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdarg.h>
 
 #include "s21_string.h"
 
@@ -8,29 +8,112 @@ int s21_sprintf(char *str, const char *format, ...) {
   size_t curr = 0;
   for (char *ptr = (char *)format; *ptr; ptr++) {
     if (*ptr != '%') {
-      str[curr] = *ptr;
+      str[curr++] = *ptr;
     } else {
       ptr++;
-      options opts;
+      options opts = {false, false, false, false, false,
+                      0,     0,     false, '\0',  '\0'};
       while (get_flag(&opts, ptr)) {
         ptr++;
       }
       ptr = get_width(args, &opts, ptr);
       ptr = get_precision(args, &opts, ptr);
       ptr = get_length(&opts, ptr);
-      opts.specifier = *ptr;
-/*       printf("Flags: %d %d %d %d %d\n", opts.left_justify, opts.with_sign,
-             opts.space, opts.hash, opts.set_zeroes);
-      printf("Width and precision: %d %d\n", opts.width, opts.precision);
-      printf("Length and specifier: %c %c\n", opts.length, opts.specifier); */
-      break;
+      opts.specifier = *ptr++;
+      handle_specifier(args, opts, str, &curr);
     }
-    curr++;
+    printf("yes\n");
   }
 
   va_end(args);
-  str++;
   return 1;
+}
+
+void handle_specifier(va_list args, options opts, char *dest, size_t *curr) {
+  char buffer[BUFFER_SIZE];
+  if (opts.specifier == 'd' || opts.specifier == 'i') {
+    parse_int(args, opts, buffer);
+  }
+
+  for (size_t i = 0; buffer[i]; i++, (*curr)++) {
+    dest[*curr] = buffer[i];
+  }
+}
+
+void parse_int(va_list args, options opts, char *dest) {
+  long num;
+  switch (opts.length) {
+    case 'l':
+      num = va_arg(args, long);
+      break;
+    case 'h':
+      num = (long)va_arg(args, long);
+      num = (short int)num;
+      break;
+    default:
+      num = (long)va_arg(args, int);
+      break;
+  }
+
+  char number[BUFFER_SIZE] = "\0";
+  parse_int_to_ascii(num, 10, number);
+  size_t chars_length = 0;
+  if ((num >= 0 && (opts.space || opts.with_sign)) || num < 0) {
+    chars_length = 1;
+  }
+  size_t num_length = s21_strlen(number);
+  if (num < 0) {
+    num_length--;
+  }
+  size_t zeroes_for_add = 0;
+  chars_length += num_length;
+  if (opts.precision_set && opts.precision > num_length) {
+    zeroes_for_add = opts.precision - num_length;
+  } else if (opts.set_zeroes && opts.width > chars_length) {
+    zeroes_for_add = opts.width - chars_length;
+  }
+  chars_length += zeroes_for_add;
+  add_int_to_string(dest, opts, chars_length, num_length, zeroes_for_add, num,
+                    number);
+}
+
+void add_int_to_string(char *dest, options opts, size_t chars_length,
+                       size_t num_length, size_t zeroes_for_add, long num,
+                       char *num_str) {
+  size_t curr = 0;
+  if (num >= 0 && opts.space && !opts.with_sign) {
+    dest[curr++] = ' ';
+  }
+
+  if (!opts.left_justify && !opts.set_zeroes) {
+    if (chars_length < opts.width) {
+      for (size_t i = opts.width - chars_length; i > 0; i--) {
+        dest[curr++] = ' ';
+      }
+    }
+  }
+
+  if (num >= 0 && opts.with_sign) {
+    dest[curr++] = '+';
+  } else if (num < 0) {
+    dest[curr++] = '-';
+    num_str++;
+  }
+
+  for (size_t i = 0; i < zeroes_for_add; i++) {
+    dest[curr++] = '0';
+  }
+  for (size_t i = 0; i < num_length; i++) {
+    dest[curr++] = num_str[i];
+  }
+
+  if (opts.left_justify && !opts.set_zeroes) {
+    if (chars_length < opts.width) {
+      for (size_t i = opts.width - chars_length; i > 0; i--) {
+        dest[curr++] = ' ';
+      }
+    }
+  }
 }
 
 bool get_flag(options *opts, char *ptr) {
@@ -63,7 +146,7 @@ char *get_width(va_list args, options *opts, char *ptr) {
     opts->width = va_arg(args, int);
     ptr++;
   } else if (ZERO_NUMBER_IN_ASCII < *ptr && *ptr <= NINE_NUMBER_IN_ASCII) {
-    opts->width = parse_num_from_ascii(&ptr);
+    opts->width = parse_int_from_ascii(&ptr);
   }
   return ptr;
 }
@@ -76,7 +159,7 @@ char *get_precision(va_list args, options *opts, char *ptr) {
       opts->precision = va_arg(args, int);
       ptr++;
     } else if (ZERO_NUMBER_IN_ASCII <= *ptr && *ptr <= NINE_NUMBER_IN_ASCII) {
-      opts->precision = parse_num_from_ascii(&ptr);
+      opts->precision = parse_int_from_ascii(&ptr);
     }
   }
   return ptr;
@@ -90,7 +173,7 @@ char *get_length(options *opts, char *ptr) {
   return ptr;
 }
 
-int parse_num_from_ascii(char **str) {
+int parse_int_from_ascii(char **str) {
   int num = ((int)**str - ZERO_NUMBER_IN_ASCII);
   (*str)++;
   while (ZERO_NUMBER_IN_ASCII <= **str && **str <= NINE_NUMBER_IN_ASCII) {
@@ -99,4 +182,30 @@ int parse_num_from_ascii(char **str) {
     (*str)++;
   }
   return num;
+}
+
+void parse_int_to_ascii(long num, int base, char *dest) {
+  char buffer[BUFFER_SIZE];
+  size_t curr = BUFFER_SIZE - 1;
+  char *digits = "0123456789abcdef";
+  bool negative = num < 0;
+  num = negative ? -num : num;
+
+  if (num == 0) {
+    dest[0] = '0';
+  }
+
+  while (num > 0) {
+    curr--;
+    buffer[curr] = digits[num % base];
+    num /= base;
+  }
+
+  size_t idx = 0;
+  if (negative) {
+    dest[idx++] = '-';
+  }
+  while (buffer[curr]) {
+    dest[idx++] = buffer[curr++];
+  }
 }
--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
 
@@ -8,8 +9,8 @@ typedef struct {
   bool hash;
   bool set_zeroes;
 
-  int width;
-  int precision;
+  size_t width;
+  size_t precision;
   bool precision_set;
 
   char length;
@@ -18,12 +19,18 @@ typedef struct {
 
 #define ZERO_NUMBER_IN_ASCII 48
 #define NINE_NUMBER_IN_ASCII 57
+#define BUFFER_SIZE 4096
 
 int s21_sprintf(char *str, const char *format, ...);
+void handle_specifier(va_list args, options opts, char *dest, size_t *curr);
 
 bool get_flag(options *opts, char *ptr);
 char *get_width(va_list args, options *opts, char *ptr);
 char *get_precision(va_list args, options *opts, char *ptr);
 char *get_length(options *opts, char *ptr);
 
-int parse_num_from_ascii(char **str);
+void parse_int(va_list args, options opts, char *buffer);
+void add_int_to_string(char *dest, options opts, size_t chars_length, size_t num_length, size_t zeroes_for_add, long num, char *num_str);
+
+int parse_int_from_ascii(char **str);
+void parse_int_to_ascii(long num, int base, char *dest);
