65e7701676e43af798824055f100d929acff02c6
AlexanderPRM <nathan920@yandex.ru>
2024-05-06 14:25:28 +0300

    fix makefile and add test.c

--- src/test.c
+++ src/test.c
@@ -86,6 +86,8 @@ Suite *s21_sprintf_suite() {
 int main() {
   int number_failed;
   SRunner *sr = srunner_create(s21_sprintf_suite());
+
+  srunner_set_fork_status(sr, CK_NOFORK);
   srunner_run_all(sr, CK_NORMAL);
   number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
